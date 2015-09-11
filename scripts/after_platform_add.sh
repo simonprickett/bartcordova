d6c839f22cf5eb4ab7372b14dbeb5a79d5294901
Simon Prickett <simon@crudworks.org>
2015-09-11 16:02:24 -0700

    Experimenting with after platform add hook.

---
+++ scripts/after_platform_add.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cordova plugin add cordova-plugin-geolocation@1.0.1
+cordova plugin add cordova-plugin-console@1.0.1
+cordova plugin add cordova-plugin-splashscreen@2.1.0
+cordova plugin add cordova-plugin-device@1.0.1
+cordova plugin add cordova-plugin-network-information@1.0.1
+cordova plugin add cordova-plugin-shake@0.5.1
+cordova plugin add https://github.com/welldone-software/cordova-statusTap
