571d85c69b8b0f55772759bd7703ed3fd8e4a731
Simon Prickett <simon@crudworks.org>
2015-09-14 08:39:45 -0700

    Adds Cordova In App Browser.

--- scripts/after_platform_add.sh
+++ scripts/after_platform_add.sh
@@ -5,5 +5,6 @@ cordova plugin add cordova-plugin-console@1.0.1
 cordova plugin add cordova-plugin-splashscreen@2.1.0
 cordova plugin add cordova-plugin-device@1.0.1
 cordova plugin add cordova-plugin-network-information@1.0.1
+cordova plugin add cordova-plugin-inappbrowser@1.0.1
 cordova plugin add cordova-plugin-shake@0.5.1
 cordova plugin add https://github.com/welldone-software/cordova-statusTap
